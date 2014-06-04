9a40be09339e5585016633b4aca5ca462e4080ff
kuc-arc-f <naka@kuc-arc-f.com>
2014-06-05 08:26:55 +0900

    0605-b, desu.
    Signed-off-by: kuc-arc-f <naka@kuc-arc-f.com>

--- top.js
+++ top.js
@@ -1,32 +1,16 @@
 //top.js
-//name= strlist
-
-var mItems = new Array();
 
 
 onload = function() {
-	init_proc();
-	
 	document.querySelector('#id-a-get').onclick = function() {
 		click_getRss();
 	}
-	
 	
 }
 
 //
 //function
 //
-function init_proc()
-{
-	chrome.storage.local.get( 'strlist' ,  function(value) {
-		if(value && value.strlist) {
-console.log( value.strlist );
-console.log( 'len='+ value.strlist.length );
-		  mItems = value.strlist;
-		}
-	});
-}
 
 function click_getRss()
 {
